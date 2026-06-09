c0e3f2eecf39dfa45a6b7396870cd499c4ace14a
Danilo-Carvalho-Antunes <danilocarvalhoantunes@gmail.com>
2026-06-09 14:57:59 -0300

    test: organiza suites por caso (Caso1Suite e Caso2Suite)
    Agrupa os testes de cada unidade em suites por categoria usando @IncludeTags, atendendo ao requisito de suite correspondente ao item.

---
+++ src/test/java/br/unb/tppe/curadoria/Caso1Suite.java
@@ -0,0 +1,11 @@
+package br.unb.tppe.curadoria;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("br.unb.tppe.curadoria")
+@IncludeTags("caso1")
+public class Caso1Suite {
+}
